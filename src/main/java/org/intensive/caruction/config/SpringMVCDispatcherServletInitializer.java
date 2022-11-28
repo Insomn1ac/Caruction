3c151ee3190ef5b35862d213a428e5763bc2a41b
Vladi207 <graschenko207@mail.ru>
2022-11-28 09:04:11 +0300

    added Spring security config

--- src/main/java/org/intensive/caruction/config/SpringMVCDispatcherServletInitializer.java
+++ src/main/java/org/intensive/caruction/config/SpringMVCDispatcherServletInitializer.java
@@ -1,22 +1,18 @@
 package org.intensive.caruction.config;
 
 import org.springframework.lang.NonNull;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 
 public class SpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class[]{WebSecurityConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SpringConfig.class, WebSecurityConfig.class};
+        return new Class[]{SpringConfig.class};
     }
 
     @Override
--- src/main/java/org/intensive/caruction/config/WebSecurityConfig.java
+++ src/main/java/org/intensive/caruction/config/WebSecurityConfig.java
@@ -2,49 +2,34 @@ package org.intensive.caruction.config;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.intensive.caruction.model.Role;
-import org.intensive.caruction.model.User;
-import org.intensive.caruction.repository.UserRepository;
-import org.intensive.caruction.util.AuthUser;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.intensive.caruction.security.JWTFilter;
+import org.intensive.caruction.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Optional;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 @Slf4j
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserRepository userRepository;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JWTFilter jwtFilter;
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsServiceBean() throws Exception {
-        return super.userDetailsServiceBean();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(email -> {
-                    log.debug("Authentication {}", email);
-                    Optional<User> optionalUser = userRepository.findByEmailIgnoreCase(email);
-                    return new AuthUser(optionalUser.orElseThrow(
-                            () -> new UsernameNotFoundException("User '" + email + "' was not found")));
-                })
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
                 .passwordEncoder(getPasswordEncoder());
     }
 
@@ -52,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/admin").hasRole(Role.ADMIN.name())
+                .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration").permitAll()
                 .and()
                 .logout()
@@ -60,6 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/auth/login")
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
