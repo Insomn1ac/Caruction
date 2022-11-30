56bb709429c15c3f95674e0a9d84847b9a454b8e
Vladi207 <graschenko207@mail.ru>
2022-11-30 15:30:55 +0300

    bugfix with WalletRepository

--- src/main/java/org/intensive/caruction/repository/WalletRepository.java
+++ src/main/java/org/intensive/caruction/repository/WalletRepository.java
@@ -2,6 +2,8 @@ package org.intensive.caruction.repository;
 
 import org.intensive.caruction.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 }
