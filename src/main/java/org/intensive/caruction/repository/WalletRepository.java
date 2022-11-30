34f96b11ef2d125f400dc1862fa4c21c22985109
eugene <emelyanoff94@gmail.com>
2022-11-30 15:01:53 +0300

    set wallet id from int to long[2]

--- src/main/java/org/intensive/caruction/repository/WalletRepository.java
+++ src/main/java/org/intensive/caruction/repository/WalletRepository.java
@@ -3,5 +3,5 @@ package org.intensive.caruction.repository;
 import org.intensive.caruction.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
 }
