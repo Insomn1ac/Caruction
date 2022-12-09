d2b049f23d6783d3109b8338d7173dddd04a4399
eugene <emelyanoff94@gmail.com>
2022-12-09 21:42:03 +0300

    delete unnecessary files

--- src/main/resources/db/scripts/006_ddl_create_table_lotRequests.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS lot_requests
-(
-    id      SERIAL PRIMARY KEY,
-    lot_id  INTEGER REFERENCES lots (id),
-    user_id INTEGER REFERENCES users (id),
-    status  VARCHAR(50)
-);
