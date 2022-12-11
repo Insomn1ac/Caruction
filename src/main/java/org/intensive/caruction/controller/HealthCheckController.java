cc7de869005cab7cccf0eddaada07799dd44eb1f
Vladi207 <graschenko207@mail.ru>
2022-12-12 02:55:12 +0300

    added Swagger

--- src/main/java/org/intensive/caruction/controller/HealthCheckController.java
+++ src/main/java/org/intensive/caruction/controller/HealthCheckController.java
@@ -3,11 +3,14 @@ package org.intensive.caruction.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/")
-    public String healthCheck() {
-        return "STATUS: OK";
+    public void healthCheck(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/index.html");
     }
 }
